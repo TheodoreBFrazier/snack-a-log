94e8caea994be9288c400076f7d0005ecff024b9
TheodoreBFrazier <theo.frazier@icloud.com>
2022-02-13 20:32:53 -0500

    Create routes for front end

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,20 @@
+//Import react router
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello, world!</h1>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/snacks" element={<Index />} />
+          <Route path="/snacks/new" element={<New />} />
+          <Route exact path="/snacks/:id" element={<Show />} />
+          <Route path="/snacks/:id/edit" element={<Edit />} />
+          <Route path="*" element={<Four0Four />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
